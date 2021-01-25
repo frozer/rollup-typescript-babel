3af25193bd45feb3492fef1b000ce933d1d04a5d
Denis Morozov <denis@data-solutions.ru>
2021-01-25 20:49:36 +0300

    Replace Babel with ESBuild

--- rollup.config.js
+++ rollup.config.js
@@ -1,48 +1,35 @@
-import commonjs from '@rollup/plugin-commonjs';
-import resolve from '@rollup/plugin-node-resolve';
-import babel from '@rollup/plugin-babel';
-import pkg from './package.json';
+import dts from 'rollup-plugin-dts'
+import esbuild from 'rollup-plugin-esbuild'
 
-const extensions = [
-  '.js', '.jsx', '.ts', '.tsx',
-];
+const name = require('./package.json').main.replace(/\.js$/, '')
 
-const name = 'RollupTypeScriptBabel';
+const bundle = config => ({
+  ...config,
+  input: 'src/index.ts',
+  external: id => !/^[./]/.test(id),
+})
 
-export default {
-  input: './src/index.ts',
-
-  // Specify here external modules which you don't want to include in your bundle (for instance: 'lodash', 'moment' etc.)
-  // https://rollupjs.org/guide/en/#external
-  external: [/@babel\/runtime/],
-
-  plugins: [
-    // Allows node_modules resolution
-    resolve({ extensions }),
-
-    // Allow bundling cjs modules. Rollup doesn't understand cjs
-    commonjs(),
-
-    // Compile TypeScript/JavaScript files
-    babel({
-      extensions,
-      babelHelpers: 'runtime',
-      include: ['src/**/*'],
-    }),
-  ],
-
-  output: [{
-    file: pkg.main,
-    format: 'cjs',
-  }, {
-    file: pkg.module,
-    format: 'es',
-  }, {
-    file: pkg.browser,
-    format: 'iife',
-    name,
-
-    // https://rollupjs.org/guide/en/#outputglobals
-    globals: {},
-  }],
-};
+export default [
+  bundle({
+    plugins: [esbuild()],
+    output: [
+      {
+        file: `${name}.js`,
+        format: 'cjs',
+        sourcemap: true,
+      },
+      {
+        file: `${name}.mjs`,
+        format: 'es',
+        sourcemap: true,
+      },
+    ],
+  }),
+  bundle({
+    plugins: [dts()],
+    output: {
+      file: `${name}.d.ts`,
+      format: 'es',
+    },
+  }),
+]
